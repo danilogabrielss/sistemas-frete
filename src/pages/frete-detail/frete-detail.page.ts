7cbf0ac6f72354d5a1ebb9f684652ee16a366a18
Danilo Gabriel <danilogabrielss15@gmail.com>
2022-11-09 01:09:15 -0300

    evoluindo a Frete-detail

--- src/pages/frete-detail/frete-detail.page.ts
+++ src/pages/frete-detail/frete-detail.page.ts
@@ -37,11 +37,20 @@ export class FreteDetailPage implements OnInit {
         this.frete.ds_cidade_destino = res.req2.nome;
         this.frete.ds_estado_origem = res.req3.sigla;
         this.frete.ds_estado_destino = res.req4.sigla;
-        var cabecalho: string = 'Origem: '+this.frete.ds_cidade_origem + '/' + this.frete.ds_estado_origem + ' Destino: ' + this.frete.ds_cidade_destino + '/' + this.frete.ds_estado_destino
-        /*+ this.frete.produto
-        {{frete.id}}
-        */
-        document.getElementById('lblCabecalhoFrete').innerHTML = cabecalho;
+
+        document.getElementById('lblOrigem').innerHTML = this.frete.ds_cidade_origem + '/' + this.frete.ds_estado_origem;
+        document.getElementById('lblDestino').innerHTML = this.frete.ds_cidade_destino + '/' + this.frete.ds_estado_destino;
+        document.getElementById('lblDescricao').innerHTML = this.frete.descricao;
+        document.getElementById('lblProduto').innerHTML = this.frete.produto;
+        document.getElementById('lblValor').innerHTML = 'R$' + this.frete.preco;
+        document.getElementById('lblRastreado').innerHTML = this.frete.rastreamento == true ? 'Sim' : 'Não';
+        document.getElementById('lblAgenciado').innerHTML = this.frete.agenciamento == true ? 'Sim' : 'Não';
+        document.getElementById('lblLona').innerHTML = this.frete.lona == true ? 'Sim' : 'Não';
+
+
+
+
+
       });
     });
   }
