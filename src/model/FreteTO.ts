4e9cbbca4505e6ca23eeec5ad82e2a6bd34bbe20
Danilo Gabriel <danilogabrielss15@gmail.com>
2022-11-09 01:09:01 -0300

    melhorando a FreteTO

--- src/model/FreteTO.ts
+++ src/model/FreteTO.ts
@@ -8,18 +8,21 @@ export class FreteTO {
     ds_estado_origem: string = '';
     estado_destino: string;
     ds_estado_destino: string = '';
+
     data_postagem: Date;
     data_coleta: string;
     data_entrega: string;
+
     descricao: string;
     preco: string;
+    peso: string;
     produto: string;
-    /*
+    tipo_carga: string;
     forma_de_pagamento: string;
     lona: boolean;
-    produto: string;
-
     ativo: boolean;
     agenciamento: boolean;
-    */
+    rastreamento: boolean;
+    pedagio: boolean;
+
 }
