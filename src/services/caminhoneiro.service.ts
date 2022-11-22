06397a7a427518664083b3068e4f34b246b2ad98
Danilo Gabriel <danilogabrielss15@gmail.com>
2022-11-21 22:18:50 -0300

    criando servico de caminhoneiro para o combo

---
+++ src/services/caminhoneiro.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { EmpresaTO } from 'src/model/EmpresaTO';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CaminhoneiroService {
+    //url: string = 'http://localhost:3030/Caminhoneiros';
+    url: string = 'https://tccfretes.herokuapp.com/Caminhoneiros';
+    constructor(private httpClient: HttpClient) { }
+
+    getCaminhoneiro() {
+        return this.httpClient.get<any[]>(this.url);
+    }
+
+    /*getCaminhoneirosByid(id) {
+      return this.httpClient.get<any>(this.url + '/' + id);
+    }
+  
+    postCaminhoneiro(empresa : EmpresaTO) {
+      return this.httpClient.post(this.url, empresa);
+    }*/
+
+}
