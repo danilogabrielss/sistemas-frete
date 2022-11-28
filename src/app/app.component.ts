cf29668fa051be8ecd4a52bce5b04e3db5ca13cd
Danilo Gabriel <danilogabrielss15@gmail.com>
2022-11-27 22:07:10 -0300

    Corrigindo menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public appPages = [
     { title: 'Tela inicial', url: '/home', icon: 'home' },
-    //{ title: 'Login para caminhoneiro', url: '/login-caminhoneiro', icon: 'log-in' },
     { title: 'Cadastro de Caminhoneiros', url: '/caminhoneiro-form', icon: 'add' },
-    { title: 'Cadastro de Frete', url: '/frete-form', icon: 'add' },
     { title: 'Listagem de Frete', url: '/frete-list', icon: 'list' },
     { title: 'Procura Frete', url: '/procura-frete', icon: 'search' },
+  ];
+  public menuEmpresas = [
+    { title: 'Cadastro de Frete', url: '/frete-form', icon: 'add' },
     { title: 'Listagem de Caminhoneiros', url: '/procura-frete-list', icon: 'list' },
   ];
   constructor() { }
